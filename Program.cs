ad95b4f7080c9903b1476d0cb3489dff605e0cff
Maryam Ariyan <maryam.ariyan@microsoft.com>
2017-09-18 16:29:22 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace odbcnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
